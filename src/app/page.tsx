0c1d549ec574657543935efefa6bfa31d331b1fa
Mahmud Apak <apakmahmud@gmail.com>
2025-04-03 22:28:33 +0200

    ref: auth is improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-'use client'
-import { useEffect } from 'react'
-
 export default function Home() {
-  useEffect(() => {
-    const audioContext = new window.AudioContext()
-    const oscillator = audioContext.createOscillator()
-    const gainNode = audioContext.createGain()
-
-    oscillator.connect(gainNode)
-    gainNode.connect(audioContext.destination)
-
-    gainNode.gain.value = 0.3 // Almost silent
-    oscillator.start()
-    setTimeout(() => {
-      oscillator.stop()
-    }, 1000) // Play for 1 second
-  }, [])
-
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <button>login</button>
+      <h1>Glorious Landing Page</h1>
     </div>
   )
 }
