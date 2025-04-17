f207612ad37749282f5908e7e66d867397c718b1
Mahmud Apak <apakmahmud@gmail.com>
2025-04-17 20:01:11 +0200

    feat: select component

--- src/components/reusables/Select/index.tsx
+++ src/components/reusables/Select/index.tsx
@@ -2,7 +2,8 @@
 
 import { useState } from 'react'
 import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
-import { CheckIcon, ChevronDown } from 'lucide-react'
+import classNames from 'classnames'
+import { CheckIcon, ChevronDown, X } from 'lucide-react'
 
 type SelectItem = {
   value: string | number
@@ -10,12 +11,14 @@ type SelectItem = {
 }
 
 export default function Select({
+  placeholder = 'Select an option',
   mode,
   options,
   label,
   value,
   onChange,
 }: {
+  placeholder?: string
   mode?: 'multiple'
   options: { value: any; label: any }[]
   label: string
@@ -39,23 +42,43 @@ export default function Select({
     <Listbox as="div" value={selected} onChange={handleChange} multiple={mode === 'multiple'}>
       <Label className="block text-sm/6 font-medium">{label}</Label>
       <div className="relative mt-1">
-        <ListboxButton className="group bg-(--bg) text-(--text) grid w-full cursor-default grid-cols-1 rounded-md py-1 px-2 text-left outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-orange-3 sm:text-sm/6">
-          <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
-            <span className="truncate block items-center gap-1">
-              {mode === 'multiple' ? (
-                (selected as SelectItem[]).map((item) => (
-                  <div key={item.value} className="inline-block bg-(--bg-secondary) px-2 py-1 mr-1 rounded">
-                    {item.label}
-                  </div>
-                ))
-              ) : (
-                <div>{(selected as SelectItem).label}</div>
-              )}
-            </span>
+        <ListboxButton
+          className={classNames(
+            'group bg-(--bg) text-(--text) w-full grid grid-cols-1 rounded-md py-1 px-2 text-left outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-orange-3 sm:text-sm/6',
+            { 'px-4': mode !== 'multiple' },
+          )}
+        >
+          <span className={classNames('col-start-1 row-start-1 flex-1 flex items-center gap-3 pr-4', { 'pr-8': mode === 'multiple' })}>
+            {(mode === 'multiple' ? (selected as SelectItem[])?.length : !!selected) ? (
+              <span className="truncate block items-center gap-1">
+                {mode === 'multiple' ? (
+                  (selected as SelectItem[]).map((item) => (
+                    <div key={item.value} className="inline-block bg-(--bg-secondary) px-2 py-1 mr-1 rounded">
+                      {item.label}
+                    </div>
+                  ))
+                ) : (
+                  <div>{(selected as SelectItem).label}</div>
+                )}
+              </span>
+            ) : (
+              <div className={classNames('opacity-50', { 'pl-2': mode === 'multiple' })}>{placeholder}</div>
+            )}
           </span>
+          {mode === 'multiple' && (selected as SelectItem[])?.length > 0 && (
+            <div
+              className="btn-text col-start-1 row-start-1 justify-self-end self-center mr-4 hover:bg-(--bg-secondary) rounded-full p-1"
+              onClick={(e) => {
+                e.stopPropagation()
+                handleChange(mode === 'multiple' ? [] : null)
+              }}
+            >
+              <X className="size-5 sm:size-4" />
+            </div>
+          )}
           <ChevronDown
             aria-hidden="true"
-            className=" col-start-1 row-start-1 size-5 self-center justify-self-end sm:size-4 transition-transform duration-200 group-data-[open]:rotate-180"
+            className="col-start-1 row-start-1 self-center justify-self-end size-5 sm:size-4 transition-transform duration-200 group-data-[open]:rotate-180"
           />
         </ListboxButton>
 
@@ -70,7 +93,7 @@ export default function Select({
               className="group relative cursor-default py-2 pr-9 pl-3 select-none data-focus:bg-orange-3 data-focus:text-white data-focus:outline-hidden"
             >
               <div className="flex items-center">
-                <span className="ml-3 block truncate font-normal group-data-selected:font-semibold">{item.label}</span>
+                <span className="ml-2 block truncate font-normal group-data-selected:font-semibold">{item.label}</span>
               </div>
 
               <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-orange-3 group-not-data-selected:hidden group-data-focus:text-white">
