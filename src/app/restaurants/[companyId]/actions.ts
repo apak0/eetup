d99f26081c74e305943f8aa86ec321bb035ec7d7
Mahmud Apak <apakmahmud@gmail.com>
2025-04-29 23:06:36 +0200

    feat: home page search

--- src/app/restaurants/[companyId]/actions.ts
+++ src/app/restaurants/[companyId]/actions.ts
@@ -4,7 +4,7 @@ import { db } from '@/lib/database/db'
 import { product } from '@/lib/database/schema'
 import { auth } from '@/lib/utils/auth'
 
-export const getProductsAction = async (companyId:number) => {
+export const getProductsAction = async (companyId: number) => {
   const session = await auth()
 
   const products = session?.user?.id ? await db.select().from(product).where(eq(product.companyId, companyId)) : []
